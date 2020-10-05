0cb3378dc993bf43c70fdcef2fc90e8282088d61
timStankevych <tim.stankevich@gmail.com>
2020-10-05 21:22:01 +0300

    added Clock.stories.tsx and Clock component Clock.tsx

---
+++ src/components/Clock/Clock.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Clock} from './Clock';
+
+export default {
+    title: 'Clock',
+    component: Clock
+    ,
+};
+
+
+export const BaseExample = () => {
+    return <Clock/>
+};
+
